036a131c6e6924e87a6e473d5beb1b96a3b6e99f
DiplomAlex <sash0k2008@rambler.ru>
2015-11-03 23:09:42 +0200

    Delete _sql & _html

--- _sql/v.1/SAGA-183_2015_02_09.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `api_history` (
-  `api_history_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id записи',
-  `api_history_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата добавления запроса',
-  `api_history_request_method` varchar(255) NOT NULL COMMENT 'Идентификатор метода запроса',
-  `api_history_request` mediumtext COLLATE utf8_unicode_ci COMMENT 'Данные запроса',
-  `api_history_response` mediumtext COLLATE utf8_unicode_ci COMMENT 'Ответ запроса',
-  PRIMARY KEY (`api_history_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Таблица истории запросов' AUTO_INCREMENT=1 ;
